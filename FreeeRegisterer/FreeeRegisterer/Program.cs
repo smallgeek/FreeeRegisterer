1b373e928e331c0fe0965dabb3293a32af8fb05e
smallgeek <smallgeek@outlook.jp>
2020-09-18 16:21:07 +0900

    プロジェクトと処理の格子を作成

---
+++ FreeeRegisterer/FreeeRegisterer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FreeeRegisterer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // プラグイン読み込み。
+
+
+            // 各ソースから登録内容を取得。
+
+
+            // freee にログイン。
+
+
+            // 未登録データのみ登録する。
+        }
+    }
+}
